30a4521a4d6924450880fa9b794d4b4598ece975
michae1bo <159921466+michae1bo@users.noreply.github.com>
2024-12-16 14:37:24 +0000

    feat: add element info output

--- element.sh
+++ element.sh
@@ -15,7 +15,7 @@ else
 	fi
 	if [[ -z $ATOMIC_NUMBER ]]
 	then 
-		echo "I could not find that element in the database"
+		echo "I could not find that element in the database."
 	else 
 		ELEMENT_NAME=$($PSQL "SELECT name FROM elements WHERE atomic_number=$ATOMIC_NUMBER")
 		ELEMENT_SYMBOL=$($PSQL "SELECT symbol FROM elements WHERE atomic_number=$ATOMIC_NUMBER")
@@ -23,5 +23,6 @@ else
 		MELTING_POINT=$($PSQL "SELECT melting_point_celsius FROM properties INNER JOIN elements USING(atomic_number) WHERE atomic_number=$ATOMIC_NUMBER")
 		BOILING_POINT=$($PSQL "SELECT boiling_point_celsius FROM properties INNER JOIN elements USING(atomic_number) WHERE atomic_number=$ATOMIC_NUMBER")
 		ELEMENT_TYPE=$($PSQL "SELECT type FROM types INNER JOIN properties USING(type_id) INNER JOIN elements USING(atomic_number) WHERE atomic_number=$ATOMIC_NUMBER")
+		echo "The element with atomic number $ATOMIC_NUMBER is $ELEMENT_NAME ($ELEMENT_SYMBOL). It's a $ELEMENT_TYPE, with a mass of $ATOMIC_MASS amu. $ELEMENT_NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
 	fi
 fi
